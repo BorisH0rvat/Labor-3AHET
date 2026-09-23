cd5eab34e1541699b6a008f28249e5687d344c08
Boris Horvat <boris.horvat@htlwy.at>
2026-09-23 15:56:07 +0200

    23. 09. 26 15:56

--- Werkzeugverwaltung/Werkzeugverwaltung/Program.cs
+++ Werkzeugverwaltung/Werkzeugverwaltung/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,52 +15,59 @@ namespace Werkzeugverwaltung
         {
             private string description;
             private int number;
-            private bool inStore;
+            private bool inStore = true;
 
             public void SetDescription(string description)
             {
                 this.description = description;
             }
-
             public void SetNumber(int number)
             {
                 this.number = number;
             }
-
+            public int GetNumber()
+            {
+                return this.number;
+            }
+            public bool GetStatus()
+            {
+                return this.inStore;
+            }
             public void SetStatus(bool inStore)
             {
                 this.inStore = inStore;
             }
             public void Print()
             {
-                Console.WriteLine("{0} {1}", description, number);
-                Console.Write("Im Lager: {0}", inStore);
+                Console.WriteLine("{0} {1} Im Lager: {2}", description, number, inStore);
             }
         }
         static void Main(string[] args)
         {
-            int x = 12;
-            Tool s1 = new Tool();
-            Tool s2 = new Tool();
-            Tool s3 = new Tool();
-            s1.SetDescription("Schraubendreher");
-            s2.SetDescription("Wasserwaage");
-            s3.SetDescription("Schraubendreher");
+            List<Tool> tools = new List<Tool>();
+            tools.Add(new Tool());
+            tools.Add(new Tool());
+            tools.Add(new Tool());
 
+
+            string toolDescription;
+            int toolNumber;
+            int searchNumber;
             
-            s1.SetNumber(8892);
-            s2.SetNumber(19828);
-            s3.SetNumber(64938);
+            tools[0].SetDescription("Schraubendreher");
+            tools[0].SetNumber(8892);
+            tools[0].SetStatus(true);
+            
 
-            s1.SetStatus(true);
-            s2.SetStatus(true);
-            s3.SetStatus(true);
+            tools[1].SetDescription("Wasserwaage");
+            tools[1].SetNumber(19828);
+            tools[1].SetStatus(true);
+            
 
-            s1.Print();
-            s2.Print();
-            s3.Print();
-
-            Console.ReadKey();
+            tools[2].SetDescription("Schraubendreher");
+            tools[2].SetNumber(64938);
+            tools[2].SetStatus(true);
+            
 
             bool stayalive = true;
             char auswahl;
@@ -76,6 +85,47 @@ namespace Werkzeugverwaltung
                 {
                     case 'B':
                     case 'b': stayalive = false; break;
+                    case 'l':
+                    case 'L':
+                        foreach (Tool t in tools)
+                        {
+                            t.Print();
+                        }
+                        break;
+                    case 'h':
+                    case 'H': 
+                        toolDescription = Console.ReadLine();
+                        toolNumber = Console.Read();
+                        tools.Add(new Tool());
+                        tools[tools.Count].SetDescription(toolDescription);
+                        tools[tools.Count].SetNumber(toolNumber);
+                        tools[tools.Count].SetStatus(true);
+                        break;
+                    case 'a':
+                    case 'A':
+                        Console.WriteLine("Ausleihen (Inventarnummer): ");
+                        searchNumber = int.Parse(Console.ReadLine());
+                        int i = 0;
+                        int foundNum;
+                        bool found = false;
+                        int number;
+                        do
+                        {
+                            if (tools[i].GetNumber() == searchNumber)
+                            {
+                                found = true;
+                                foundNum = i;
+                            }
+                        } while ((i <= tools.Count) || (found = false));
+                        if (found = false)
+                        {
+                            Console.WriteLine("Es wurde nichts gefunden.");
+                        }
+                        else if (found  = true)
+                        {
+
+                        }
+                            break;
                 }
             } while (stayalive == true);
         }
